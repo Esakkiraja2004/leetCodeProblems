d3fb7657b3ccd7dc91ab175f92e152d3fe45b30e
Esakkiraja2004 <esakkiraja2401@gmail.com>
2024-10-08 23:47:48 +0530

    UFFFF

--- array/containDuplicate.py
+++ array/containDuplicate.py
@@ -1,6 +1,8 @@
-a = [1,2,3,1,2,3]
-k = 2
+a = [13,23,1,2,3]
+a.sort()
+k = 5
 l = 0
+s =set(a)
 for i in range(len(a)):
     for j in range(i+1,len(a)):
         if a[i] == a[j]:
@@ -8,7 +10,7 @@ for i in range(len(a)):
             print(f"Indices: {i}, {j}")  
             break
 print(l)
-if l <= k and len(a) >= l:
+if l <= k and len(a) >= l  and a != list(s) :
     print(True)
 else:
     print(False)
