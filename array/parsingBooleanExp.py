cf2af23882f1bbc713800f1e31fd0b23b41fbb81
Esakkiraja2004 <esakkiraja2401@gmail.com>
2024-10-20 22:27:37 +0530

    !@#$%^&*

--- array/parsingBooleanExp.py
+++ array/parsingBooleanExp.py
@@ -1,1 +1,9 @@
 e = "&(|(f))"
+arr = [x for x in e]
+boo =  [y  for y in arr if y == 't' or y == 'f']
+exp = [x for x in arr if x == '!' or x == '|' or x == '&' ]
+while exp :
+    op = exp.pop()
+    if op == '|':
+        while boo :
+            
