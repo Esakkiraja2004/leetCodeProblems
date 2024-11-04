cd7b8653f7b7d116d382d708de3f7a5f06851ee8
Esakkiraja2004 <esakkiraja2401@gmail.com>
2024-11-04 22:52:29 +0530

    Challenge done

--- array/strCompressionIII.py
+++ array/strCompressionIII.py
@@ -1,13 +1,12 @@
-class Solution:
-    def compressedString(self, word):
-        comp = []
+word = "aaaabbb"
+comp = []
+count = 1
+        
+for i in range(1, len(word) + 1):
+    if i == len(word) or word[i] != word[i - 1] or count == 9:
+        comp.append(str(count) + word[i - 1])
         count = 1
-        
-        for i in range(1, len(word) + 1):
-            if i == len(word) or word[i] != word[i - 1] or count == 9:
-                comp.append(str(count) + word[i - 1])
-                count = 1
-            else:
-                count += 1
-        
-        return ''.join(comp)
+    else:
+        count += 1
+        # return ''.join(comp)
+print(comp)
