b93c2aa33fb58acaf55aef256144450c4af1ac98
Esakkiraja2004 <esakkiraja2401@gmail.com>
2025-02-06 23:31:15 +0530

    Extra challenge Done

--- Arrays and Strings/containDuplicate.py
+++ Arrays and Strings/containDuplicate.py
@@ -1,16 +1,11 @@
-a = [13,23,1,2,3]
-a.sort()
-k = 5
-l = 0
-s =set(a)
-for i in range(len(a)):
-    for j in range(i+1,len(a)):
-        if a[i] == a[j]:
-            l = j - i
-            print(f"Indices: {i}, {j}")  
-            break
-print(l)
-if l <= k and len(a) >= l  and a != list(s) :
-    print(True)
-else:
-    print(False)
+nums =[1,0,1,1]
+k = 1
+hashmap ={}
+
+for i , num in enumerate(nums):
+    print(i , num)
+    if num in hashmap :
+        if abs(i - hashmap[num]) <= k:
+            print(True)
+    hashmap[num] = i
+print(hashmap)
