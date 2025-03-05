67ae1a5706e713ba7273cf83a6919cbf35be38e7
Esakkiraja2004 <esakkiraja2401@gmail.com>
2025-03-05 23:07:37 +0530

    Challenge Done

---
+++ Arrays and Strings/countTotalNumOfcolorCell.py
@@ -0,0 +1,7 @@
+n = 2
+k = 0
+s = 1
+while k < n:
+    s += (k*4)
+    k+=1
+print(s)
---
+++ Arrays and Strings/validAnagaram.py
@@ -0,0 +1,7 @@
+s = "anagaram"
+
+t = "nagaram"
+
+print(sorted(s) == sorted(t))
+print(sorted(s))
+print(sorted(t))
