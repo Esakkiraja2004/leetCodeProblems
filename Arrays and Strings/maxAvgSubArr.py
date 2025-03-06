88ed5abc09077d1147aebaadceff6ee6fac05450
Esakkiraja2004 <esakkiraja2401@gmail.com>
2025-03-06 22:58:20 +0530

    challenge Done

---
+++ Arrays and Strings/maxAvgSubArr.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: float
+        """
+        start = 0
+        end = 0
+        n = len(nums)
+        sum = 0
+        max_avg = float('-inf')
+        while end < n:
+            if k>(end-start):
+                sum += nums[end]
+            if k==(end-start+1):
+                max_avg = max(max_avg,sum)
+                sum -= nums[start]
+                start+=1
+            end+=1
+        return max_avg/float(k)
