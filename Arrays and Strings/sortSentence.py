1f443d179b7af5eccc90396e0444965d4a6409bd
Esakkiraja2004 <esakkiraja2401@gmail.com>
2025-03-27 22:47:05 +0530

    challlenge Done

--- Arrays and Strings/sortSentence.py
+++ Arrays and Strings/sortSentence.py
@@ -1,9 +1,9 @@
-s = "is2 sentence4 This1 a3"
+s = "z1 z2 z3"
 arr = s.split(" ")
 d = {}
 for i in arr:
-    c = i[:-1]
-    d[c] = int(i[-1])
+    num = int(i[-1])
+    d[i]= num
 sorted_words = sorted(d.keys(), key=d.get)
 
 sorted_sentence = " ".join(word[:-1] for word in sorted_words)
